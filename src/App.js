c48f423e0923db232dd7bea19e98fa6675987bb8
chad hinds <chadrickj8@gmail.com>
2023-02-13 14:08:34 -0500

    Setup of initial API call and image import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './Poke-logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+// URL to get the the first 10 generation 1 pokemon
+const pokeCall = "https://pokeapi.co/api/v2/pokemon?limit=10";
+
+
 function App() {
+  // State
+  const [pokemonList, setPokemonList] = useState([]);
+
+
   return (
     <>
+      {/* NB: This is how you get the images from public folder */}
+      {/* <img src='/Charmander.png' alt='charmander, the fire type starter from generation one' /> */}
       mic check
     </>
   );
