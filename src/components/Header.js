dd1080d78bab09ea01912dceb936ea49ed2048bf
chad hinds <chadrickj8@gmail.com>
2023-02-21 13:22:54 -0500

    include Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../Poke-logo.svg';
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="container">
+            <img src={logo} className="img-fluid App-logo" alt="Pokeball" width='70px' />
         </div>
     );
 };
