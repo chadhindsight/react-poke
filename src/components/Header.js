a599b688a66a14a95a41f2e11ea7dbc51d6224a9
chad hinds <chadrickj8@gmail.com>
2023-02-23 07:32:33 -0500

    Include a span in the header for img

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container Header">
-                <h1 className="display-4">Pokewall <img src={logo} className="img-fluid App-logo center" alt="Pokeball" width='65px' /></h1>
+                <h1 className="display-4">Pokewall <span><img src={logo} className="img-fluid App-logo center" alt="Pokeball" width='65px' /></span></h1>
                 <p className="lead">A place to learn about the original three starter Pokemon.</p>
             </div>
         </div>
