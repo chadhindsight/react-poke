d0bc152bde3180066448891c11bd8f1d91f84348
chad hinds <chadrickj8@gmail.com>
2023-03-14 14:24:33 -0400

    Remove unused imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { charMock } from './mocks/charMock'
-
-// You might also need to import Moves and Types components and pass in dummy data to them 
-import Moves from './components/Moves';
-import Types from './components/Types';
-import PokeCard from './components/PokeCard';
 
 
-// NB: Delete this test eventually
+
 test('renders something', () => {
   render(<App />);
   const linkElement = screen.getByText(/Pokewall/i);
