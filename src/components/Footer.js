590b84459773a947e33d45da69d8381dbe48c00d
chad hinds <chadrickj8@gmail.com>
2023-03-19 13:15:51 -0400

    Change text in footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,8 @@ const Footer = () => {
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li c="nav-item"><a href="https://www.linkedin.com/in/chadrick-hinds" className="nav-link px-2 text-muted">Linkedin</a></li>
                 <li className="nav-item"><a href="https://github.com/chadhindsight" className="nav-link px-2 text-muted">Github</a></li>
-                <li className="nav-item"><a href="http://chadhindsight.github.io" className="nav-link px-2 text-muted">Portfolio Site</a></li>
+                <li className="nav-item"><a href="http://chadhindsight.github.io" className="nav-link px-2 text-muted">My Portfolio Site</a></li>
             </ul>
-            dsfsdf
             <p className="text-center text-muted">© {new Date().getFullYear()} Chad Hinds</p>
         </footer>
     );
