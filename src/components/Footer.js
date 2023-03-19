e88b8e00d50853fec96b16edbd488c3536ebcf8e
chad hinds <chadrickj8@gmail.com>
2023-03-19 13:47:17 -0400

    fix typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="py-3 my-4">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                <li c="nav-item"><a href="https://www.linkedin.com/in/chadrick-hinds" target="_blank" rel='noreferrer' className="nav-link px-2 text-muted">Linkedin</a></li>
+                <li className="nav-item"><a href="https://www.linkedin.com/in/chadrick-hinds" target="_blank" rel='noreferrer' className="nav-link px-2 text-muted">Linkedin</a></li>
                 <li className="nav-item"><a href="https://github.com/chadhindsight" target="_blank" rel='noreferrer' className="nav-link px-2 text-muted">Github</a></li>
                 <li className="nav-item"><a href="http://chadhindsight.github.io" target="_blank" rel='noreferrer' className="nav-link px-2 text-muted">My Portfolio Site</a></li>
             </ul>
