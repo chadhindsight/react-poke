3d7ab5d43685ff91729aa210e834c3f92d65403a
chad hinds <chadrickj8@gmail.com>
2023-03-26 11:19:12 -0400

    Make initial pokecard adjustment

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,33 +1,40 @@
 import React from 'react';
-import { Container, Button, Col, Card } from 'react-bootstrap';
+// import { Container, Button, Col, Card } from 'react-bootstrap';
+import Card from "@mui/material/Card";
+import CardContent from '@mui/material/CardContent';
+import Button from "@mui/material/Button";
 import Moves from './PokemonInfo/Moves';
 import Types from './PokemonInfo/Types';
+import { CardMedia, Grid, Typography } from '@mui/material';
 
 const PokeCard = ({ selectedPokemon, getPokemonInfo, pokemon }) => {
 
     return (
-        <Container align="center" className="container-sm mt-4">
-            <Col>
-                <Card>
-                    <Card.Body>
-                        <Card.Img variant="top" src={`/${pokemon?.name}.png`} alt={`${pokemon?.name}, one of the starters from generation one`} />
-                        <h2>{pokemon?.name.toUpperCase()}</h2>
-                        <hr />
 
-                        {
-                            selectedPokemon?.name === pokemon?.name ?
-                                <>
-                                    <Moves moves={selectedPokemon?.moves} />
-                                    <hr />
-                                    <Types types={selectedPokemon?.types} />
-                                </>
-                                :
-                                <Button onClick={() => getPokemonInfo(pokemon.url)}>Learn More</Button>
-                        }
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Container>
+        <Card>
+            <CardContent>
+                <CardMedia
+                    sx={{ height: 350, width: 350 }}
+                    image={`/${pokemon?.name}.png`}
+                    title={`${pokemon?.name}, one of the starters from generation one`}
+                />
+                <hr />
+                <Typography gutterBottom variant="h4" component="div">
+                    {pokemon?.name.toUpperCase()}
+                </Typography>
+
+                {
+                    selectedPokemon?.name === pokemon?.name ?
+                        <>
+                            <Moves moves={selectedPokemon?.moves} />
+                            <hr />
+                            <Types types={selectedPokemon?.types} />
+                        </>
+                        :
+                        <Button onClick={() => getPokemonInfo(pokemon.url)}>Learn More</Button>
+                }
+            </CardContent>
+        </Card>
     );
 };
 
